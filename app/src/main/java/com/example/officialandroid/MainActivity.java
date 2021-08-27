68789a1fe426ef5ab218aadcef6595eff8b89a10
foty <534728972@qq.com>
2021-08-27 10:16:22 +0800

    viewpager2添加依赖

--- app/src/main/java/com/example/officialandroid/MainActivity.java
+++ app/src/main/java/com/example/officialandroid/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.officialandroid;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv1 = findViewById(R.id.tvViewPager2);
+        tv1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvViewPager2:
+                startActivity(new Intent(this, ViewPager2ClassActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/officialandroid/ViewPager2ClassActivity.java
@@ -0,0 +1,20 @@
+package com.example.officialandroid;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Create by lxx
+ * Date : 2021/8/27 9:49
+ * Use by
+ */
+public class ViewPager2ClassActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
